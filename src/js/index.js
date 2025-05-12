31bb08b1e9ff2c561162247784afccc947d766f6
João Pedro <166664369+JaoPred0@users.noreply.github.com>
2025-05-12 15:49:23 -0300

    Novo desing no home e no jao

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,6 @@
-const scriptBootstrap = document.createElement('script');
-scriptBootstrap.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js';
-document.head.appendChild(scriptBootstrap);
-
-// 🚀 AOS (Animate On Scroll) - Efeitos de animação ao rolar a página
-const scriptAOS = document.createElement('script');
-scriptAOS.src = 'https://unpkg.com/aos@next/dist/aos.js';
-document.head.appendChild(scriptAOS);
-
-// 🎡 Swiper - Biblioteca para criação de carrosséis e sliders
-const scriptSwiper = document.createElement('script');
-scriptSwiper.src = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js';
-document.head.appendChild(scriptSwiper);
+const a = document.createElement('script');
+a.src = 'https://cdn.jsdelivr.net/npm/@tabler/core@1.2.0/dist/js/tabler.min.js';
+document.head.appendChild(a);
 
 // 🔄 Inicializar AOS após o carregamento do script
 scriptAOS.onload = () => {
